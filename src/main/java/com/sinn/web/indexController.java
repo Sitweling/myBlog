00fe0024e81b9e1533386abd660a84108f3036df
Sitweling <937019004@qq.com>
2022-04-07 14:33:52 +0800

    增加日志处理，获得url,ip,方法名字和参数

--- src/main/java/com/sinn/web/indexController.java
+++ src/main/java/com/sinn/web/indexController.java
@@ -3,6 +3,7 @@ package com.sinn.web;
 import com.sinn.exception.NotFoundBlogException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * @Description:
@@ -11,13 +12,14 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class indexController {
-    @GetMapping("/")
-    public String index(){
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable Integer id,@PathVariable String name){
 //        int i=9/0;
-        String blog=null ;
+/*        String blog=null ;
         if(blog==null){
             throw  new NotFoundBlogException("博客不存在");
-        }
+        }*/
+        System.out.println("--------index--------");
         return "index";
     }
 }
